df78499bc3d59b834f7eb6aa52cd354aec78a997
mlambert73 <mark_l_lambert@yahoo.com>
2021-11-22 16:04:12 -0800

    load env vars

--- src/test/java/com/applitools/quickstarts/AppTest.java
+++ src/test/java/com/applitools/quickstarts/AppTest.java
@@ -25,17 +25,22 @@ public class AppTest {
 
 	public static void main(String[] args) {
 		// BaseURL of the website
-		String baseURL = "https://demo.applitools.com";
-		//String baseURL = "https://demo.applitools.com/index_v2.html";
-		
-		// ⭐️ Note to see visual bugs, run the test using the above URL for the 1st run.
-		// but then change the above URL to https://demo.applitools.com/index_v2.html
-		// (for the 2nd run)
+		String baseURL = System.getenv("BASEURL");
+		if (baseURL == null) {
+			baseURL = "https://demo.applitools.com";
+			//baseURL = "https://demo.applitools.com/index_v2.html";
+			// ⭐️ Note to see visual bugs, run the test using the above URL for the 1st run.
+			// but then change the above URL to https://demo.applitools.com/index_v2.html
+			// (for the 2nd run)
+		}
 		
 		// Use or not-use the Ultrafast Grid
-		//boolean useGrid = false;
-		boolean useGrid = true;
-
+		boolean useGrid = false; //default to not using the Ultrafast Grid (override with env var)
+		String env = System.getenv("USE_GRID");
+		if (env != null) {
+			useGrid = return Boolean.parseBoolean(env);
+		}
+		
 		// Create a new chrome web driver
 		WebDriver webDriver = new ChromeDriver(new ChromeOptions().setHeadless(getCI()));
 
